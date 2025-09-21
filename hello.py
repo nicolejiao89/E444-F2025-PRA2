d8dd4cb87f53ee5027ccc7f2a188114d1b9f7654
Nicole Jiao <carrie@ruojins-mbp.home>
2025-09-21 11:42:02 -0400

    activity 1.3 completed

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_moment import Moment
+
 app = Flask(__name__) 
-@app.route('/') 
-def index(): 
-    return '<h1>Hello World!</h1>'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.route('/user/<name>') 
+def user(name):
+    return render_template("user.html", name=name, current_time=datetime.utcnow())
